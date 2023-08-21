91530f36293db57e127f16804a8d1d28bdbd6a34
Serebus26 <serhat.akr26@gmail.com>
2023-08-22 02:38:28 +0300

    Interface

---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonelLoanManager personelLoanManager = new PersonelLoanManager();
+            AutoLoanManager autoLoanManager = new AutoLoanManager();
+            MortgageLoanManager mortgageLoanManager = new MortgageLoanManager();
+
+            ApplicationManager applicationManager = new ApplicationManager();
+            applicationManager.Apply(personelLoanManager);
+            applicationManager.Apply(autoLoanManager);
+            applicationManager.Apply(mortgageLoanManager);
+
+            List<ICreditManager> credits = new List<ICreditManager>() { personelLoanManager, autoLoanManager, mortgageLoanManager };
+            applicationManager.KrediOnBilgilendirmesiYap(credits);
+            
+
+        }
+    }
+}
